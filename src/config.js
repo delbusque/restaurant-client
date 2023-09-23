f108078d7cc9eeed5bff816663b9b6802276f925
Ventsislav Peychev <del.busque@gmail.com>
2023-09-23 16:58:49 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const baseUrl = 'https://restaurant-api-5xzz.onrender.com'
+// export const baseUrl = 'https://restaurant-api-5xzz.onrender.com'
+export const baseUrl = 'http://localhost:5000'
