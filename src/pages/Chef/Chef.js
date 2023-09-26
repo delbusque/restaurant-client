d6cb2359737f7d87a3d9e514635af5386cc846d5
Ventsislav Peychev <del.busque@gmail.com>
2023-09-26 12:17:31 +0300

    Changed orders order upon window width

--- src/pages/Chef/Chef.js
+++ src/pages/Chef/Chef.js
@@ -11,13 +11,25 @@ const Chef = () => {
 
     return (
         <div className={styles["chef-orders"]}>
-            <div className={styles["ready"]}>
-                {readyData?.sort((a, b) => b.updatedAt.localeCompare(a.updatedAt)).map(order => <ReadyOrder orders={readyData} key={order._id} ready={order} refetch={refetch} />)}
+            {window.innerWidth > 900 ?
+                <>
+                    <div className={styles["ready"]}>
+                        {readyData?.sort((a, b) => b.updatedAt.localeCompare(a.updatedAt)).map(order => <ReadyOrder orders={readyData} key={order._id} ready={order} refetch={refetch} />)}
 
-            </div>
-            <div className={styles["waiting"]}>
-                {data?.map(order => order.waiting && <ChefOrder data={data} orders={waitingData} key={order._id} waiting={order} refetch={refetch} />)}
-            </div>
+                    </div>
+                    <div className={styles["waiting"]}>
+                        {data?.map(order => order.waiting && <ChefOrder data={data} orders={waitingData} key={order._id} waiting={order} refetch={refetch} />)}
+                    </div>
+                </>
+                : <>
+                    <div className={styles["waiting"]}>
+                        {data?.map(order => order.waiting && <ChefOrder data={data} orders={waitingData} key={order._id} waiting={order} refetch={refetch} />)}
+                    </div>
+                    <div className={styles["ready"]}>
+                        {readyData?.sort((a, b) => b.updatedAt.localeCompare(a.updatedAt)).map(order => <ReadyOrder orders={readyData} key={order._id} ready={order} refetch={refetch} />)}
+
+                    </div>
+                </>}
         </div>
     )
 }
