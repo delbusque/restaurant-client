6c42e40d229fccad93804d8d8b106680addad23f
delbusque <del.busque@gmail.com>
2023-10-06 17:08:06 +0300

    Fix Home page image issue

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import styles from './Home.module.css'
 const Home = () => {
     return (
         <div className={styles['image']}>
-            <div className={styles['title']}>Welcome to <span>Deli</span> management system</div>
+            <div className={styles['title']}>Welcome to <span>Deli</span></div>
         </div>
     )
 }
