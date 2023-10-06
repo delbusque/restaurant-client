875b38842dfeeb29b559582c734b1ad7fa8cd6bb
delbusque <del.busque@gmail.com>
2023-10-06 17:07:06 +0300

    Change responsive tables

--- src/components/Tables/Tables.js
+++ src/components/Tables/Tables.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 import { useAuthContext } from "../../hooks/useAuthContext";
 import TableError from './TableError';
+import { RiTakeawayLine } from 'react-icons/ri'
 
 const Tables = ({ tables }) => {
 
@@ -11,27 +12,34 @@ const Tables = ({ tables }) => {
         <>
             {user &&
                 <section className="tables-cont">
-                    {tables && tables.map(t => t.type === 'table' && (
-                        <Link id='table-num' to={user ? t.number : null} className="links" key={t._id}>
-                            {t.paid && <div className={t.opened ? "table-btn-opened" : "table-btn"} >
-                                <p className="table-btn-text">{t.number}</p>
-                            </div>}
-                            {!t.paid && <div className={t.opened ? "table-btn-unpaid" : "table-btn"} >
-                                <p className="table-btn-text">{t.number}</p>
-                            </div>}
-                        </Link>
-                    ))}
-                    <br />
-                    {tables && tables.map(t => t.type === 'away' && (
-                        <Link id='table-num' to={user ? t.number : null} className="links" key={t._id}>
-                            {t.paid && <div className={t.opened ? "table-btn-opened away" : "table-btn away"} >
-                                <p className="table-btn-text">{t.number}</p>
-                            </div>}
-                            {!t.paid && <div className={t.opened ? "table-btn-unpaid away" : "table-btn away"} >
-                                <p className="table-btn-text">{t.number}</p>
-                            </div>}
-                        </Link>
-                    ))}
+                    <div className="inside-cont">
+                        {tables && tables.map(t => t.type === 'table' && (
+                            <Link to={user ? t.number : null} className="links" key={t._id}>
+                                {t.paid && <div className={t.opened ? "table-btn opened" : "table-btn"} >
+                                    <p className="table-btn-text">{t.number}</p>
+                                </div>}
+                                {!t.paid && <div className={t.opened ? "table-btn unpaid" : "table-btn"} >
+                                    <p className="table-btn-text">{t.number}</p>
+                                </div>}
+                            </Link>
+                        ))}
+                        <div className="table-btn add"><p className="table-btn-text">+</p></div>
+                    </div>
+
+
+                    <div className="inside-cont away">
+                        {tables && tables.map(t => t.type === 'away' && (
+                            <Link to={user ? t.number : null} className="links" key={t._id}>
+                                {t.paid && <div className={t.opened ? "table-btn opened away" : "table-btn away"} >
+                                    <p className="table-btn-text">{t.number}</p>
+                                </div>}
+                                {!t.paid && <div className={t.opened ? "table-btn unpaid away" : "table-btn away"} >
+                                    <p className="table-btn-text">{t.number}</p>
+                                </div>}
+                            </Link>
+                        ))}
+                        <div className="table-btn add"><p className="table-btn-text">+</p></div>
+                    </div>
                 </section >
             }
             {!user && <TableError />}
