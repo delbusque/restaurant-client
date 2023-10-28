8d5665cf7a6492ea4b45fbfb82efe59fc8963def
Ventsislav Peychev <del.busque@gmail.com>
2023-10-28 11:53:48 +0300

    Change ui language

--- src/components/Tables/TableError.js
+++ src/components/Tables/TableError.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const TableError = () => {
     return (
-        <div className="table-error">You are not authorized and must <Link id='err-login' to='/login' className={styles['err-login']}> login</Link> or <Link id='err-signup' to='/signup' className={styles['err-signup']}> sign up</Link> to proceed with Tables !</div>
+        <div className="table-error">Не сте оторизиран и трябва да се <Link id='err-login' to='/login' className={styles['err-login']}> впишете</Link> или <Link id='err-signup' to='/signup' className={styles['err-signup']}> регистрирате</Link> за да продължите напред !</div>
     )
 }
 
