ebe5b94849719783165b75f683bf47ec7251bf44
delbusque <del.busque@gmail.com>
2023-11-13 15:57:57 +0200

    Fix table owner view buttons on mobile screen size

--- src/components/Tables/Order.js
+++ src/components/Tables/Order.js
@@ -52,13 +52,13 @@ const Order = ({ order, addItemHandler, deleteItemHandler, tableNum, table, setT
                 : <>
                     <div className={styles['ord-name']}>{order.name}</div>
                     < div className={styles['tb-orders']} >
-                        {user?.role !== 5051 && <button className={styles['button-53']} onClick={() => deleteItemHandler(order)}>-</button>}
-                        {user?.role !== 5051 && <button className={styles['button-53-green']} onClick={() => addItemHandler(order)}>+</button>}
+                        {(user?.role !== 5051 && user.id === tableOwner?._id) && <button className={styles['button-53']} onClick={() => deleteItemHandler(order)}>-</button>}
+                        {(user?.role !== 5051 && user.id === tableOwner?._id) && <button className={styles['button-53-green']} onClick={() => addItemHandler(order)}>+</button>}
                         <div className={styles['ord-count']}>
                             <span className={styles['ord-counter']}>{order.count} </span> x {order.price.toFixed(2)}
                         </div>
 
-                        {(order.family === 'food' && user?.role !== 5051) && <button className={styles['button-53-blue']} onClick={toChefHandler}> {sent} / {count}</button>}
+                        {(order.family === 'food' && user?.role !== 5051 && user.id === tableOwner?._id) && <button className={styles['button-53-blue']} onClick={toChefHandler}> {sent} / {count}</button>}
 
                         <div className={styles['ord-total']}>{(order.count * order.price).toFixed(2)}
                             <span className={styles['lv']}>лв.</span>
