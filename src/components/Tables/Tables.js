6cebc6d3b1b305762b1e9b26ae650bd0d85a5b03
delbusque <del.busque@gmail.com>
2023-11-15 11:52:54 +0200

    Differentiate other from own tables

--- src/components/Tables/Tables.js
+++ src/components/Tables/Tables.js
@@ -1,27 +1,11 @@
-import { useState, useContext, useEffect } from 'react';
-
 import { Link } from 'react-router-dom';
 
 import { useAuthContext } from "../../hooks/useAuthContext";
 import TableError from './TableError';
 
-import { baseUrl } from '../../config.js';
-import { useQuery } from 'react-query'
-import axios from 'axios';
-
 const Tables = ({ tables }) => {
 
     const { user } = useAuthContext();
-
-    const fetchUsers = () => axios.get(`${baseUrl}/user`)
-
-    const { data } = useQuery('users', fetchUsers, {
-        select: data => data.data,
-        refetchOnWindowFocus: false,
-    })
-
-    console.log(data);
-    console.log(tables);
 
     return (
         <>
@@ -30,12 +14,20 @@ const Tables = ({ tables }) => {
                     <div className="inside-cont">
                         {tables && tables.map(t => t.type === 'table' && (
                             <Link to={user ? t.number : null} className="links" key={t._id}>
-                                {t.paid && <div className={t.opened ? "table-btn opened" : "table-btn"} >
-                                    <p className="table-btn-text">{t.number}</p>
-                                </div>}
-                                {!t.paid && <div className={t.opened ? "table-btn unpaid" : "table-btn"} >
-                                    <p className="table-btn-text">{t.number}</p>
-                                </div>}
+                                {t.paid &&
+                                    <div className={t.ownerId === user.id ? '' : 'other'}>
+                                        <div className={t.opened ? "table-btn opened" : "table-btn"} >
+                                            <p className="table-btn-text">{t.number}</p>
+                                        </div>
+                                    </div>
+                                }
+                                {!t.paid &&
+                                    <div className={t.ownerId === user.id ? '' : !t.opened ? '' : 'other'}>
+                                        <div className={t.opened ? "table-btn unpaid" : "table-btn"} >
+                                            <p className="table-btn-text">{t.number}</p>
+                                        </div>
+                                    </div>
+                                }
                             </Link>
                         ))}
                         <div className="table-btn add"><p className="table-btn-text">+</p></div>
@@ -45,12 +37,19 @@ const Tables = ({ tables }) => {
                     <div className="inside-cont away">
                         {tables && tables.map(t => t.type === 'away' && (
                             <Link to={user ? t.number : null} className="links" key={t._id}>
-                                {t.paid && <div className={t.opened ? "table-btn opened away" : "table-btn away"} >
-                                    <p className="table-btn-text">{t.number}</p>
-                                </div>}
-                                {!t.paid && <div className={t.opened ? "table-btn unpaid away" : "table-btn away"} >
-                                    <p className="table-btn-text">{t.number}</p>
-                                </div>}
+                                {t.paid &&
+                                    <div className={t.ownerId === user.id ? '' : 'other'}>
+                                        <div className={t.opened ? "table-btn opened away" : "table-btn away"} >
+                                            <p className="table-btn-text">{t.number}</p>
+                                        </div>
+                                    </div>
+                                }
+                                {!t.paid &&
+                                    <div className={t.ownerId === user.id ? '' : !t.opened ? '' : 'other'}>
+                                        <div className={t.opened ? "table-btn unpaid away" : "table-btn away"} >
+                                            <p className="table-btn-text">{t.number}</p>
+                                        </div>
+                                    </div>}
                             </Link>
                         ))}
                         <div className="table-btn add"><p className="table-btn-text">+</p></div>
