e166ef21b73bda0fac50db69f92f380ebaa5f153
Ventsislav Peychev <del.busque@gmail.com>
2025-01-27 13:15:44 +0200

    Change open table functionality
    Change open table functionality when clicking on the table number. Add open handler fetching tables data and checking if current table is already opened

--- src/components/Tables/TableCard.js
+++ src/components/Tables/TableCard.js
@@ -4,8 +4,9 @@ import { RiTakeawayLine } from 'react-icons/ri'
 import { useAuthContext } from '../../hooks/useAuthContext.js';
 import axios from 'axios';
 import { baseUrl } from '../../config.js';
+import * as apiService from './../../services/apiService.js'
 
-const TableCard = ({ table, setTables, addItemHandler, deleteItemHandler, tableOwner }) => {
+const TableCard = ({ table, setTables, addItemHandler, deleteItemHandler, tableOwner, number }) => {
 
     const { user } = useAuthContext();
 
@@ -40,6 +41,26 @@ const TableCard = ({ table, setTables, addItemHandler, deleteItemHandler, tableO
         navigate('/tables')
     }
 
+    const openHandler = () => {
+        if (user.role !== 5051) {
+            apiService.fetchTables().then(data => {
+                let [{ ...currTable }] = data.filter(t => t.number == number)
+                window.localStorage.setItem('currTable', JSON.stringify(currTable))
+                return currTable
+            }).then(currTable => {
+                if (!currTable.opened) {
+                    table.opened = true;
+                    table.ownerId = user.id;
+                    setTables(oldState => [...oldState], table);
+                    window.localStorage.setItem('currTable', JSON.stringify(table))
+
+                    axios.post(`${baseUrl}/tables/edit/${table._id}`, { table })
+                }
+            });
+        }
+    }
+
+
     return (
         <section className={!table.paid ? 'orders-sect' : 'orders-sect-paid'}>
             <div className="tb-head">
@@ -48,17 +69,31 @@ const TableCard = ({ table, setTables, addItemHandler, deleteItemHandler, tableO
 
                 {table.ownerId && <div className='tb-title firstName'>{tableOwner?.firstName || tableOwner?.email}</div>}
                 {table.paid && <button className='btn-green'>ПЛАТЕНО</button>}
-                <div className='tb-num'>{table.number}</div>
+                {table.opened ? <div className='tb-num-op'>{table.number}</div> : <div className='tb-num' onClick={openHandler}>{table.number}</div>}
+
             </div>
 
             <div className='ord-footer'>
                 <div className='tb-foot'>СМЕТКА</div>
                 <div className='tb-total'>{totalSum.toFixed(2)} <span className='tb-total-lv'>лв.</span></div>
 
-                {table.opened && <div className="btn-cont">
+                {/* {table.opened && <div className="btn-cont">
                     {(user.role !== 5051 && user.id === tableOwner?._id) && !table.paid
                         ? <button className={table.type == 'table' ? (table.orders.length > 0 ? 'btn-paid' : 'btn-dis') : (table.orders.length > 0 ? 'btn-paid-ta' : 'btn-dis')} onClick={payHandler}>ПЛАТИ</button>
                         : <button className='btn-clear' onClick={clearHandler}>ИЗЧИСТИ</button>
+                    }
+                </div>
+                } */}
+                {(table.opened && table.orders.length > 0 && user.id === tableOwner?._id) && <div className="btn-cont">
+                    {(user.role !== 5051 && user.id === tableOwner?._id) && !table.paid
+                        ? <button className={table.type == 'table' ? (table.orders.length > 0 ? 'btn-paid' : 'btn-dis') : (table.orders.length > 0 ? 'btn-paid-ta' : 'btn-dis')} onClick={payHandler}>ПЛАТИ</button>
+                        : <button className={table.type == 'table' ? 'btn-clear' : 'btn-clear-ta'} onClick={clearHandler}>ИЗЧИСТИ</button>
+                    }
+                </div>
+                }
+                {(table.opened && table.orders.length < 1) && <div className="btn-cont">
+                    {(user.role !== 5051 && user.id === tableOwner?._id) && table.opened
+                        && <button className={table.type == 'table' ? 'btn-clear' : 'btn-clear-ta'} onClick={clearHandler}>ИЗЧИСТИ</button>
                     }
                 </div>
                 }
