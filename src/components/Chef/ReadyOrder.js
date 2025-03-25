2851f356beecb84b3c44a8cc4564af038bde659a
delbusque <del.busque@gmail.com>
2025-03-25 17:01:24 +0200

    Change button title

--- src/components/Chef/ReadyOrder.js
+++ src/components/Chef/ReadyOrder.js
@@ -46,7 +46,7 @@ const ReadyOrder = ({ ready, refetch, orders }) => {
 
                 </div>
                 {(user?.role === 1984 || user?.role === 402) &&
-                    <button className={styles['order-ready']} onClick={() => deleteReadyOrder(ready)}>ИЗТРИЙ</button>
+                    <button className={styles['order-ready']} onClick={() => deleteReadyOrder(ready)}>ДАДЕНА</button>
                 }
 
             </div>
