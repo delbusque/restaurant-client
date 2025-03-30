118d954f204374f39ca12958e37ba30de554545d
Ventsislav Peychev <del.busque@gmail.com>
2025-03-30 15:30:54 +0300

    Add Cancel button for input edit

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -30,6 +30,15 @@ const Account = () => {
         }));
     };
 
+    const handleCancel = () => {
+        setEditingField('');
+        setFormData({
+            name: '',
+            lastName: '',
+            phone: ''
+        });
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         
@@ -74,6 +83,9 @@ const Account = () => {
                             <button type="submit" className={styles['save-btn']}>
                                 <i className="fa-solid fa-check"></i>
                             </button>
+                            <button type="button" onClick={handleCancel} className={styles['cancel-btn']}>
+                                <i className="fa-solid fa-xmark"></i>
+                            </button>
                         </form>
                     ) : (
                         <div className={styles['acc__name-name']}>{user.name}</div>
@@ -96,6 +108,9 @@ const Account = () => {
                             />
                             <button type="submit" className={styles['save-btn']}>
                                 <i className="fa-solid fa-check"></i>
+                            </button>
+                            <button type="button" onClick={handleCancel} className={styles['cancel-btn']}>
+                                <i className="fa-solid fa-xmark"></i>
                             </button>
                         </form>
                     ) : (
@@ -120,6 +135,9 @@ const Account = () => {
                             <button type="submit" className={styles['save-btn']}>
                                 <i className="fa-solid fa-check"></i>
                             </button>
+                            <button type="button" onClick={handleCancel} className={styles['cancel-btn']}>
+                                <i className="fa-solid fa-xmark"></i>
+                            </button>
                         </form>
                     ) : (
                         <div className={styles['acc__name-name']}>{user.phone}</div>
@@ -136,6 +154,34 @@ const Account = () => {
             </div>
 
             <div className={styles['posts-cont']}>
+                <div className={styles["blog-list-row"]}>
+                    <div className={styles["left-info"]}>
+                        <div className={styles["author-cont"]}>
+                            <div className={styles["post-author-name"]}>by {user.firstName} {user.lastName}</div>
+                            {user &&
+                                <div className={styles["post-icons"]}>
+                                    <button className={styles['edit']} >
+                                        <i className="fa-solid fa-marker marker"></i>
+                                    </button>
+                                    <button className={styles['delete']} >
+                                        <i className="fa-solid fa-trash-arrow-up trash"></i>
+                                    </button>
+                                </div>
+                            }
+                        </div>
+                        <div className={styles["date-published"]}>August 12, 2016</div>
+                    </div>
+
+                    <div className={styles["post-text"]}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lectus
+                        massa. Pellentesque ornare mauris a auctor pellentesque. Sed sit amet
+                        metus at odio venenatis elemen mauris. Fusce mauris
+                        nibh, gravida eu eros sed, suscipit sollicitudin dui. Etiam tellus
+                        justo, fringilla at tempor in, mollis et felis.{" "}
+                    </div>
+
+                </div>
+
                 <div className={styles["blog-list-row"]}>
                     <div className={styles["left-info"]}>
                         <div className={styles["author-cont"]}>
