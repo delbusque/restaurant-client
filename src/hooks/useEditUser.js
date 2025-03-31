33377ad634f170bc307dbf84569153d36631c830
Ventsislav Peychev <del.busque@gmail.com>
2025-03-31 11:49:43 +0300

    Update useEditUser.js

--- src/hooks/useEditUser.js
+++ src/hooks/useEditUser.js
@@ -11,6 +11,7 @@ export const useEditUser = () => {
     const { dispatch } = useAuthContext();
 
     const editUser = async (name, lastName, email, phone) => {
+        
         setIsLoading(true);
         setError(null);
 
