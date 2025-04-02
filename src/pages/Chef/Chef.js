954bc38593fb834e942b7788c1eae831dbbba6dc
Ventsislav Peychev <del.busque@gmail.com>
2025-04-02 15:35:12 +0300

    Separate fryer orders from standard kitchen orders

--- src/pages/Chef/Chef.js
+++ src/pages/Chef/Chef.js
@@ -16,11 +16,11 @@ const Chef = () => {
             {window.innerWidth > 900 ?
                 <>
                     <div className={styles["ready"]}>
-                        {readyData?.sort((a, b) => a.createdAt.localeCompare(b.createdAt)).map(order => <ReadyOrder orders={readyData} key={order._id} ready={order} refetch={refetch} />)}
+                        {readyData?.sort((a, b) => a.createdAt.localeCompare(b.createdAt)).map(order => !order.fryer && <ReadyOrder orders={readyData} key={order._id} ready={order} refetch={refetch} />)}
 
                     </div>
                     <div className={styles["waiting"]}>
-                        {data?.map(order => order.waiting && <ChefOrder data={data} orders={waitingData} key={order._id} waiting={order} refetch={refetch} />)}
+                        {data?.map(order => (order.waiting && !order.fryer) && <ChefOrder data={data} orders={waitingData} key={order._id} waiting={order} refetch={refetch} />)}
                     </div>
                 </>
                 : user.role !== 402 ?
@@ -29,17 +29,17 @@ const Chef = () => {
                             {data?.map(order => (order.waiting && !order.fryer) && <ChefOrder data={data} orders={waitingData} key={order._id} waiting={order} refetch={refetch} />)}
                         </div>
                         <div className={styles["ready"]}>
-                            {readyData?.sort((a, b) => a.createdAt.localeCompare(b.createdAt)).map(order => <ReadyOrder orders={readyData} key={order._id} ready={order} refetch={refetch} />)}
+                            {readyData?.sort((a, b) => a.createdAt.localeCompare(b.createdAt)).map(order => !order.fryer && <ReadyOrder orders={readyData} key={order._id} ready={order} refetch={refetch} />)}
 
                         </div>
                     </> :
                     <>
                         <div className={styles["ready"]}>
-                            {readyData?.sort((a, b) => a.createdAt.localeCompare(b.createdAt)).map(order => <ReadyOrder orders={readyData} key={order._id} ready={order} refetch={refetch} />)}
+                            {readyData?.sort((a, b) => a.createdAt.localeCompare(b.createdAt)).map(order => !order.fryer && <ReadyOrder orders={readyData} key={order._id} ready={order} refetch={refetch} />)}
 
                         </div>
                         <div className={styles["waiting"]}>
-                            {data?.map(order => order.waiting && <ChefOrder data={data} orders={waitingData} key={order._id} waiting={order} refetch={refetch} />)}
+                            {data?.map(order => (order.waiting && !order.fryer) && <ChefOrder data={data} orders={waitingData} key={order._id} waiting={order} refetch={refetch} />)}
                         </div>
 
                     </>}
